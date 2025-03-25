472dbfe98a1cfea55d1f1cdba57a9835a246221f
Nicolas GIRARD <NG38@outlook.fr>
2025-03-25 12:11:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+
+const Vianel = "Vianel";
+const David = "David";
+const Hans = "Hans";
+const Jordan = "Jordan";
+console.log(
+	chalk.blue(`${Vianel}`) +
+		chalk.red(`${David}`) +
+		chalk.green(`${Hans}`) +
+		chalk.yellow(`${Jordan}`),
+);
